8234811e72961d1060166d192f10b00f7445b229
George Alvarado <georgealvarado@outlook.com>
2022-03-17 17:01:24 -0500

    comment line code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export addNums(num1, num2) {
-    if(num1 === undefined || num2 === undefined) return false
+    //if(num1 === undefined || num2 === undefined) return false
     
     return num1 + num2;
 }
