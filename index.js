e7f192483e868c3b5f7257628163ffcb94907e32
George Alvarado <georgealvarado@outlook.com>
2022-03-17 17:00:38 -0500

    added null validator

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export addNums(num1, num2) {
-    if(num1 === undefined || num2 === undefined) return false
+    if(num1 === undefined || null || num2 === undefined || null) return false
     
     return num1 + num2;
 }
